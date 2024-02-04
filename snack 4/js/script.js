31f373e43f11d30638a83b014a30f228f5ad27a1
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-04 01:36:32 +0100

    fine snack 4

---
+++ snack 4/js/script.js
@@ -0,0 +1,28 @@
+
+let myButton = document.querySelector(".btn").addEventListener("click",
+    function () {
+        let inputElement = document.getElementById("userChoice");
+        let inputValue = inputElement.value;
+
+        if (!isNaN(inputValue) && inputValue.length === 4) {
+            let sum = 0;
+
+            for (let i = 0; i < inputValue.length; i++) {
+                sum += +inputValue[i];
+
+            }
+
+            let resultElement = document.querySelector("p");
+
+
+            resultElement.innerHTML = "";
+
+            resultElement.innerHTML = "La somma delle cifre è: " + sum;
+
+        } else {
+            alert("Inserisci un numero di 4 cifre valido.");
+        }
+    }
+
+
+);
